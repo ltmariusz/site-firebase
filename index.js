c0a2f9968b00b4a88ce15a919dd0874d6743789d
ltmariusz <ltmariuszl@gmail.com>
2024-02-14 15:14:03 +0100

    little change

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 // Funkcja do ładowania Google Maps API
 function loadGoogleMapsAPI() {
-    var script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_MAPS_API_KEY}&callback=initMap`;
-    script.defer = true;
-    document.head.appendChild(script);
+	var script = document.createElement('script');
+	script.src = `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_MAPS_API_KEY}&callback=initMap`;
+	script.defer = true;
+	document.head.appendChild(script);
 }
 
 // Wywołaj funkcję loadGoogleMapsAPI() po załadowaniu strony
 window.onload = loadGoogleMapsAPI;
 
 
-function wyswietlanie(){
+function wyswietlanie() {
+
 	var szero = document.getElementById("szero");
 	var dlug = document.getElementById("dlug");
 	var bater = document.getElementById("bater");
@@ -19,7 +20,7 @@ function wyswietlanie(){
 	//gdzie jest użytkownik
 	//szerokość geograficzna
 	var szeroRef = firebase.database().ref().child("latitude");
-	szeroRef.on('value', function(datasnapshot){
+	szeroRef.on('value', function (datasnapshot) {
 		szero.innerText = datasnapshot.val();
 	});
 
@@ -27,16 +28,23 @@ function wyswietlanie(){
 
 	var dlugRef = firebase.database().ref().child("longitude");
 
-	dlugRef.on('value', function(datasnapshot){
+	dlugRef.on('value', function (datasnapshot) {
 		dlug.innerText = datasnapshot.val();
 	});
 
 	//Bateria
 	var batRef = firebase.database().ref().child("batLevel");
 
-	batRef.on('value', function(datasnapshot){
+	batRef.on('value', function (datasnapshot) {
 		bater.innerText = datasnapshot.val();
 	});
-	setTimeout("wyswietlanie()",1000);
+	setTimeout("wyswietlanie()", 1000);
+}
+function init() {
+	loadGoogleMapsAPI();
+	wyswietlanie();
 }
 
+window.onload = init;
+
+window.addEventListener('load', init);
